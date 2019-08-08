bbad1fbaf5744bf8a76c509ea142b09b069340ed
Hidran Arias <hidran@gmail.com>
2019-08-08 19:07:23 +0200

    16. Adds users array to users component and shows users

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -7,5 +7,43 @@ import { Component } from '@angular/core';
 })
 
 export  class UsersComponent {
-
+ title = 'Users'
+ users = [
+  {
+      name: 'Hidran1',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+  } ,
+  {
+      name: 'Hidran2',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+  },
+  {
+      name: 'Hidran3',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+  },
+  {
+      name: 'Hidran4',
+      lastname: 'Arias',
+      email: 'hidran@gmail.com',
+      fiscalcode: 'RSAHRN72M22Z444S',
+      province: 'Torino',
+      phone: '454545455',
+      age: 43
+  }
+];
 }
