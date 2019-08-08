b80830fe18750a967f7dc3f524e41bd25ddd0986
Hidran Arias <hidran@gmail.com>
2019-08-08 18:24:37 +0200

    creates users component

---
+++ src/app/users/users.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector : 'app-users',
+  templateUrl:'./users.component.html',
+  styleUrls : ['./users.component.css']
+})
+
+export  class UsersComponent {
+
+}
