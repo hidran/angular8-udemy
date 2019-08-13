b759f63ccc93109669a31ced4c2175e01e1d50da
Hidran Arias <hidran@gmail.com>
2019-08-13 16:38:47 +0200

    36.Adds bootstrap css

--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit, EventEmitter, Output} from '@angular/core';
 import {UserService} from '../services/user.service';
-import {User} from '../interfaces/user';
+import {User} from '../classes/user';
 
 @Component({
   selector: 'app-users',
