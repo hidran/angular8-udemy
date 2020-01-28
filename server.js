3dca2fbb50ce72e6657fa1faa7914ce96f2175bc
Hidran <hidran@gmail.com>
2020-01-29 00:43:48 +0100

    changes server.js

--- server.js
+++ server.js
@@ -3,11 +3,11 @@ const path = require('path');
 const app = express();
 
 // Serve static files....
-app.use(express.static(__dirname + '/dist/UMS'));
+app.use(express.static(__dirname + '/dist/'));
 
 // Send all requests to index.html
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname + '/dist/UMS/index.html'));
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
 });
 
 // default Heroku PORT
