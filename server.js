c50e31c43caafbe62ea386c28a18bf78f55d53a7
Hidran <hidran@gmail.com>
2020-01-28 23:32:51 +0100

    adds node server

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+
+// Serve static files....
+app.use(express.static(__dirname + '/dist/UMS'));
+
+// Send all requests to index.html
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/UMS/index.html'));
+});
+
+// default Heroku PORT
+app.listen(process.env.PORT || 3000);
